72fe6a35eb084928996f627958fd12862767fda0
Water Smith <WaterTheDev@protonmail.com>
2021-04-15 13:09:59 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+
+os.system("pip install discord")
+os.system("cls")
+
+# dont change these credits cuz the tool is under MIT license :)
+
+os.system("title " + "Made by water1597 on TikTok [-] https://github.com/WaterTheDev/Discord-Streaming-Status")
+
+
+# change what ever the fuck u want under here just nothing from up here :)
+
+import discord
+from discord.ext import commands
+import time
+
+
+bot = commands.Bot(command_prefix=['stream ', 'Stream ', 'stream', 'Stream'], self_bot=True)
+
+
+
+
+@bot.event
+async def on_ready():
+    print("Bot is ready!")
+    await bot.change_presence(activity=discord.Streaming(name="@water1597 on tiktok is sooooooo cool :)", url="https://www.youtube.com/watch?v=hIYiovihEeU"))
+# THIS ONLY WORKS WITH TWITCH AND YOUTUBE LINKS!
+
+
+
+# do streamstop to stop the fake stream
+@bot.command(aliases=["stop"])
+async def close(ctx):
+    await bot.close()
+    print("Bot Closed")
+    print("Closing in 10 seconds :)")
+    time.sleep(10)
+
+
+bot.run('TOKEN HERE', bot = False)
