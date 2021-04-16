dd95c756d13389862f67243b42075610bb966d77
Water Smith <WaterTheDev@protonmail.com>
2021-04-16 10:47:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+# oh ur checking the src?
+# feel free!
+# https://github.com/WaterTheDev/Discord-Streaming-Status
 import os
 
-os.system("pip install discord")
-os.system("cls")
+os.system("python -m pip install discord") # some mfs dont have pip installed lmfao
+os.system("cls") # dont call me dumb if u use linux
 
 # dont change these credits cuz the tool is under MIT license :)
 
@@ -10,31 +13,86 @@ os.system("title " + "Made by water1597 on TikTok [-] https://github.com/WaterTh
 
 # change what ever the fuck u want under here just nothing from up here :)
 
+
 import discord
 from discord.ext import commands
 import time
+import json
+
+with open('config.json') as f:
+    config = json.load(f)
+
+token = config.get('token')
+prefix = config.get('prefix')
+streamurl = config.get('streamurl')
+
+bot = commands.Bot(command_prefix=prefix, self_bot=True)
 
 
-bot = commands.Bot(command_prefix=['stream ', 'Stream ', 'stream', 'Stream'], self_bot=True)
-
-
-
+# THIS ONLY WORKS WITH TWITCH AND YOUTUBE LINKS!
+# THIS ONLY WORKS WITH TWITCH AND YOUTUBE LINKS!
+bot.remove_command('help')
 
 @bot.event
 async def on_ready():
     print("Streaming status started!")
-    await bot.change_presence(activity=discord.Streaming(name="@water1597 on tiktok is sooooooo cool :)", url="https://www.youtube.com/watch?v=dQw4w9WgXcQ"))
+    print("Prefix: "+ prefix)
 # THIS ONLY WORKS WITH TWITCH AND YOUTUBE LINKS!
 
+@bot.command(aliases=["streaming"])
+async def stream(ctx, *, message):
+    await ctx.message.delete()
+    print("Status Changed.")
+    stream = discord.Streaming(
+        name=message,
+        url=streamurl,
+    )
+    await bot.change_presence(activity=stream)
 
+@bot.command(aliases=["playing"])
+async def game(ctx, *, message):
+    await ctx.message.delete()
+    print("Status Changed.")
+    game = discord.Game(
+        name=message
+    )
+    await bot.change_presence(activity=game)
+                   
+@bot.command(aliases=["listen"])
+async def listening(ctx, *, message):
+    await ctx.message.delete()
+    print("Status Changed.")
+    await bot.change_presence(
+        activity=discord.Activity(
+            type=discord.ActivityType.listening,
+            name=message,
+        ))
+
+@bot.command(aliases=["watch"])
+async def watching(ctx, *, message):
+    await ctx.message.delete()
+    print("Status Changed.")
+    await bot.change_presence(
+        activity=discord.Activity(
+            type=discord.ActivityType.watching,
+            name=message
+        ))
 
 # do streamstop to stop the fake stream
-@bot.command(aliases=["stop"])
+@bot.command(aliases=["stop", "reset"])
 async def close(ctx):
-    await bot.close()
-    print("Bot Closed")
-    print("Closing in 10 seconds :)")
+    await ctx.message.delete()
+    await bot.change_presence(activity=None, status=discord.Status.dnd)
+    print("Status reset")
     time.sleep(10)
 
+@bot.command()
+async def help(ctx):
+  await ctx.message.delete()
+  await ctx.message.channel.send("```Commands: \nstream <Status Message> - Changes your status to Streaming\nwatch <Status Message> - Sets your status to watching\ngame <Status Message> - Changes your status to Playing\nlisten <Status Message> - Sets your status to Listing to\nreset - Resets your status```")
 
-bot.run('TOKEN HERE', bot = False)
+bot.run(token, bot = False)
+
+
+# YOU MAY GET BANNED USING THIS.
+# DISCORD ToS WON'T ALLOW IT!
